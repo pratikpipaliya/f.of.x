4c8f4862271b2932fcd8cba5c5b110d22010da0e
rp46 <parikhr8@gmail.com>
2018-01-20 20:07:57 +0530

    Start of the Beginning

---
+++ Assets/scripts/boundry.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boundry : MonoBehaviour {
+	private Camera mainCam;
+	private BoxCollider2D topWall;
+	private BoxCollider2D bottomWall;
+	private BoxCollider2D leftWall;
+	private BoxCollider2D rightWall;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/scripts/playerMovements.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovements : MonoBehaviour {
+	
+	public float playspeed = 20.0f;
+
+	void Update()
+	{
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			Debug.Log ("LeftArrow key pressed.");
+			this.transform.Translate (new Vector2 (-playspeed * Time.deltaTime,0));
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			Debug.Log ("RightArrow key pressed.");
+			this.transform.Translate (new Vector2 (playspeed * Time.deltaTime,0));
+		}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			Debug.Log ("UpArrow key pressed.");
+			this.transform.Translate ( new Vector2( 0,playspeed*Time.deltaTime ) );
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			Debug.Log ("DownArrow key pressed.");
+			this.transform.Translate ( new Vector2( 0,-playspeed*Time.deltaTime ) );
+		}
+	}
+}
